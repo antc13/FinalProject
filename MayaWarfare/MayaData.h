9bdebab41254f2c0aa41352e975948d767faa788
Teejoon <jofrwito@hotmail.com>
2015-10-10 15:47:22 +0200

    Fixed transform rotation

--- MayaWarfare/MayaData.h
+++ MayaWarfare/MayaData.h
@@ -12,7 +12,7 @@ public:
 
 	MessageType read();
 	void getNewMesh(char*& name, VertexLayout*& vertecies, UINT64& numVertecies, UINT*& indecies, UINT64& numIndecies);
-	void getNewTransform(char*& name, float*& translation, float*& scale, float*& rotation1, float*& rotation2, float*& rotation3, float*& rotation4);
+	void getNewTransform(char*& name, float*& translation, float*& scale, float*& rotation1);
 private:
 	SharedMemory shared;
 	char* data;
